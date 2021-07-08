3223d7047ef009ede9a9b2ab5f607e8576745f00
Mrbrown400 <khbrown400@gmail.com>
2021-07-08 07:00:55 -0700

    info updates

--- src/components/About.js
+++ src/components/About.js
@@ -6,10 +6,10 @@ export default function About(props) {
         var name = props.data.name;
         var profilepic = 'images/' + props.data.image;
         var bio = props.data.bio;
-        var street = props.data.address.street;
+        // var street = props.data.address.street;
         var city = props.data.address.city;
         var state = props.data.address.state;
-        var phone = props.data.phone;
+        // var phone = props.data.phone;
         var email = props.data.email;
         var resumeDownload = props.data.resumedownload;
     }
@@ -29,10 +29,9 @@ export default function About(props) {
                             <h2>Contact Details</h2>
                             <p className="address">
                                 <span>{name}</span><br />
-                                <span>{street}<br />
+                                <span>{"Currently Based in:"}
                                     {city} {state}
                                 </span><br />
-                                <span>{phone}</span><br />
                                 <span>{email}</span>
                             </p>
                         </div>
